e58c3eb8c1bc161d88ca88d9a5bbaf4d0330b18e
miv-dev <i@mike39.ru>
2024-11-23 08:13:15 +0300

    Create sql with tables tools and suppliers

---
+++ flyway/sql/V1_1__create_tools.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS suppliers
+(
+    id            INTEGER PRIMARY KEY,
+    name          VARCHAR(255) UNIQUE NOT NULL,
+    address       TEXT,
+    delivery_time INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS tool_types
+(
+    id   INTEGER PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS tools
+(
+    id            INTEGER PRIMARY KEY,
+    name          VARCHAR(255) UNIQUE NOT NULL,
+    description   TEXT,
+    supplier_id   INTEGER REFERENCES suppliers ON DELETE CASCADE,
+    wear          INTEGER DEFAULT 0,
+    type_id       INTEGER REFERENCES tool_types ON DELETE CASCADE,
+    purchase_date DATE,
+    quantity      INTEGER DEFAULT 0
+);
